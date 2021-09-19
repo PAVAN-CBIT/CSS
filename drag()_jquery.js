2d9117309d1f582a2c686ea00f3a0280f5d9bfad
Pavan <69111120+PAVAN-CBIT@users.noreply.github.com>
2021-09-19 15:25:25 +0530

    Create drag()_jquery.js

---
+++ drag()_jquery.js
@@ -0,0 +1,39 @@
+<html>
+<head>
+<title>JQUERY</title>
+<script type="text/javascript" src="https://code.jquery.com/jquery-3.3.1.min.js"</script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui-min.js"
+             integrity="sha256-VazP97ZCwtekAsvgPBSUwpFkdrwD3unUfSGVYrahUqU="
+             crossorigin="anonymous"></script>
+             <link rel="stylesheet" href="https://code.jquery.com/ui/1.10.4/themes/ui-lightness/jquery-ui.css">
+    
+
+<style>
+
+#drag
+{
+width: 150px;
+height: 60px;
+background-color: green;
+}
+
+</style>
+</head>
+<body>
+
+<h1>J Q U E R Y...!</h1>
+
+<div id="drag">
+    <p>DRAG</p>
+</div>
+
+
+
+</body>
+
+
+
+
+
+
+</html>
